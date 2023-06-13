ce265030537681024d11e9b62327820420227d62
rebok dev <68105407+rebokdev@users.noreply.github.com>
2023-06-13 18:41:56 +0200

    version 0.0.0

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "dev.rebok"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
